ee8db356d70c79fff19053938920d7f6df9981c8
Stephen Montague <me@montaguestephen.com>
2022-01-05 14:56:19 -0500

    Created 2 portals for our backdrop and our error modal

--- src/Components/UI/ErrorModal.jsx
+++ src/Components/UI/ErrorModal.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import Button from './Button';
 import Card from './Card';
 import './ErrorModal.css';
 
-function ErrorModal({ title, message, onConfirm }) {
-	function Backdrop({ onConfirm }) {
-		<div className='backdrop' onClick={onConfirm}></div>;
-	}
+function Backdrop({ onConfirm }) {
+	return (
+		<div className='backdrop' onClick={onConfirm}></div>
+	);
+}
 
-	function ModalOverlay({ title, message }) {
+function ModalOverlay({ title, message, onConfirm }) {
+	return (
 		<Card className='modal'>
 			<header className='header'>
 				<h2>{title}</h2>
@@ -19,14 +23,24 @@ function ErrorModal({ title, message, onConfirm }) {
 			<footer className='footer'>
 				<Button onClick={onConfirm}>Okay</Button>
 			</footer>
-		</Card>;
-	}
+		</Card>
+	);
+}
 
+function ErrorModal({ title, message, onConfirm }) {
 	return (
 		<>
 			{ReactDOM.createPortal(
 				<Backdrop onConfirm={onConfirm} />,
 				document.getElementById('backdrop-root')
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title={title}
+					message={message}
+					onConfirm={onConfirm}
+				/>,
+				document.getElementById('overlay-root')
 			)}
 		</>
 	);
