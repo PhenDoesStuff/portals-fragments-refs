8c0c735ffe34f1f803d297e4ebd1eb5744d4314e
Stephen Montague <me@montaguestephen.com>
2022-01-05 12:08:45 -0500

    Creation of a new component to manage a list of users.

---
+++ src/Components/Users/UsersList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../UI/Card';
+import './UsersList.css';
+
+function UsersList({ users }) {
+	return (
+		<Card className='usersList'>
+			<ul>
+				{users.map(user => (
+					<li>
+						{user.name} ({user.age} years old)
+					</li>
+				))}
+			</ul>
+		</Card>
+	);
+}
+
+export default UsersList;
