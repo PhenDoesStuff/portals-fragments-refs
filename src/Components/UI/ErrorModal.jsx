b51afdfebbdd738c21aeb199f387920efedc3fea
Stephen Montague <me@montaguestephen.com>
2022-01-05 14:42:41 -0500

    Created a backdrop component and a new ErrorModal component and moved all of the code outside of the return statement

--- src/Components/UI/ErrorModal.jsx
+++ src/Components/UI/ErrorModal.jsx
@@ -4,22 +4,25 @@ import Card from './Card';
 import './ErrorModal.css';
 
 function ErrorModal({ title, message, onConfirm }) {
-	return (
-		<>
-			<div className='backdrop' onClick={onConfirm}></div>
-			<Card className='modal'>
-				<header className='header'>
-					<h2>{title}</h2>
-				</header>
-				<div className='content'>
-					<p>{message}</p>
-				</div>
-				<footer className='footer'>
-					<Button onClick={onConfirm}>Okay</Button>
-				</footer>
-			</Card>
-		</>
-	);
+	function Backdrop({ onConfirm }) {
+		<div className='backdrop' onClick={onConfirm}></div>;
+	}
+
+	function ErrorModal({ title, message }) {
+		<Card className='modal'>
+			<header className='header'>
+				<h2>{title}</h2>
+			</header>
+			<div className='content'>
+				<p>{message}</p>
+			</div>
+			<footer className='footer'>
+				<Button onClick={onConfirm}>Okay</Button>
+			</footer>
+		</Card>;
+	}
+
+	return <></>;
 }
 
 export default ErrorModal;
